35b9e1099e875cca478e1580d1e69c05758cf63c
Amanda Sharmaine Remo <asremo@amandas-mbp.lan>
2022-10-02 12:23:53 -0400

    established store, developed slice, added thunk, added functionality to buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
-import Nav from './components/Nav'
-import ContentWrapper from './components/ContentWrapper'
-import Footer from './components/Footer'
+import { useSelector, useDispatch } from 'react-redux'
+import {clearData, incrementId, decrementId, inputId, fetchData} from './features/dataSlice';
+// import { render } from '@testing-library/react';
 
 function App() {
-  
+  // your logic goes here!
+  const dispatch = useDispatch()
+  const data = useSelector((state) => state.data)
+
+  const renderImg = () => {
+    if(data.apiData) {
+      return <img style={{'width': '80vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    } else {
+      return <p>No image found</p>
+    }
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-      <Nav />
-      <ContentWrapper />
-      <Footer />
+    <div className="App">
+      <div>
+        <button onClick={() => dispatch(fetchData())}>Thunk!</button>
+        <button onClick={() => dispatch(clearData())}>Clear</button>
+        <button onClick={() => dispatch(incrementId())}>Next</button>
+        <button onClick={() => dispatch(decrementId())}>Back</button>
+      </div>
+      <input value={data.objectId} onChange={(e) => { dispatch(inputId(Number(e.target.value))) }} />
+      <div>
+        {/* Once you have plugged everything in, render the image here! */}
+        {renderImg()}
+      </div>
     </div>
   );
 }
---
+++ src/features/dataSlice.js
@@ -0,0 +1,55 @@
+//  The primary reason for this particular 
+// instance of state will be to store API data.
+
+// https://collectionapi.metmuseum.org/public/collection/v1/objects/[insert an object id here]
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    objectId: 10245, // to drive API results
+    apiData: {} // to store the actual data from the API
+}
+
+export const dataSlice = createSlice({
+    // Setting API data to state
+        name: 'data',
+        initialState,
+        reducers: {
+            setData: (state, action) => {
+                return {...state, apiData : action.payload}
+            },
+            // Re-setting state back to initial values
+            clearData: () => {
+                return initialState
+            },
+            // Incrementing the ID by one
+            incrementId: (state) => {
+                return {...state, objectId : state.objectId + 1}
+            },
+
+            // Decrementing the ID by one
+            decrementId: (state) => {
+                return {...state, objectId : state.objectId - 1}
+            },
+
+            // Entering a custom ID
+            inputId: (state, action) => {
+                return {...state, objectId : action.payload}
+            }
+        }  
+})
+
+export const {setData, clearData, incrementId, decrementId, inputId} = dataSlice.actions
+
+export const fetchData = () => {
+    const fetchDataThunk = async (dispatch, getState) => {
+        let state = getState()
+        const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${state.data.objectId}`)
+        const resData = await response.json()
+        dispatch(setData(resData))
+    }
+    return fetchDataThunk
+}
+
+
+export default dataSlice.reducer
